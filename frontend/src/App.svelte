56359e1b4418d5ea0406b90344813f8f898ac3bb
Anton Shostak <60942970+ShostakAnton@users.noreply.github.com>
2020-07-20 20:37:32 +0300

    add svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	import FormApp from './FormApp.svelte'
+    // export let name;
+
+    let subjectTwo = '';
+    let messageTwo = '';
+
+    function print(event) {
+	    subjectTwo = event.detail.subject;
+	    messageTwo = event.detail.message;
+	}
+
+</script>
+
+<main>
+	<FormApp on:formMess="{print}"/>
+    <hr>
+    <h2>{subjectTwo}</h2>
+    <p>{messageTwo}</p>
+
+<!--    <h1>Hello {name}!</h1>-->
+<!--    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>-->
+</main>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
---
+++ frontend/src/FormApp.svelte
@@ -0,0 +1,44 @@
+<script>
+    import {createEventDispatcher, onMount} from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    let subject = '';
+    let message = '';
+    let adverts = '';
+
+    $: test = subject + ", " + message
+
+    function sendForm() {
+        dispatch('formMess', {
+            subject: subject + " Test!",
+            message: message + "!!!"
+        });
+    }
+
+    onMount(async () => {
+        const res = await fetch(`http://127.0.0.1:8000/`);
+        let text = await res.json();
+        adverts = text.results
+    });
+</script>
+
+<main>
+    <form>
+        <input type="text" name="subject" bind:value={subject}>
+        <input type="text" name="message" bind:value={message}>
+        <button type="button" on:click="{sendForm}">Send</button>
+    </form>
+
+    {#each adverts as advert}
+        <p>{advert.subject}</p>
+    {:else}
+        <p>loading...</p>
+    {/each}
+
+    <p>{test}</p>
+</main>
+
+<style>
+
+</style>
